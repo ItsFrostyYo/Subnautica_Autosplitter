aa46fbaeb5b24443b2c4f03a4c484f8e6b5712c2
Sprinter31 <alfret008@web.de>
2025-10-17 20:43:40 +0200

    add voxif

---
+++ Livesplit.Subnautica/LiveSplit.VoxSplitter/Factory.cs
@@ -0,0 +1,21 @@
+﻿using LiveSplit.Model;
+using LiveSplit.NewSuperLuckysTale;
+using LiveSplit.UI.Components;
+using LiveSplit.VoxSplitter;
+using System;
+using System.Reflection;
+
+namespace LiveSplit.VoxSplitter {
+    public class Factory : IComponentFactory {
+        public string UpdateName => ComponentName;
+        public string UpdateURL => ExAssembly.GitMainURL();
+        public string XMLURL => UpdateURL + "Components/ComponentsUpdate.xml";
+        public Version Version => ExAssembly.GetName().Version;
+        public string ComponentName => ExAssembly.FullComponentName();
+        public string Description => ExAssembly.Description();
+        public ComponentCategory Category => ComponentCategory.Control;
+        public IComponent Create(LiveSplitState state) => new NewSuperLuckysTaleComponent(state);
+
+        public static Assembly ExAssembly = Assembly.GetExecutingAssembly();
+    }
+}
