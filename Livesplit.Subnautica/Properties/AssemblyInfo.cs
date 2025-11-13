b0fc2ee0b3e77741916965f120bdb3ec9aa38333
Sprinter31 <alfret008@web.de>
2025-11-13 20:52:43 +0100

    better ency translations

--- Livesplit.Subnautica/Properties/AssemblyInfo.cs
+++ Livesplit.Subnautica/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.2.1.4")]
-[assembly: AssemblyFileVersion("2.2.1.4")]
+[assembly: AssemblyVersion("2.2.1.5")]
+[assembly: AssemblyFileVersion("2.2.1.5")]
 
 [assembly: ComponentFactory(typeof(Factory))]
