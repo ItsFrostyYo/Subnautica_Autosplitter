a7789fa9799a47eb12bb8726474fab3615320f70
Sprinter31 <alfret008@web.de>
2025-12-06 14:38:03 +0100

    update

--- Livesplit.Subnautica/Properties/AssemblyInfo.cs
+++ Livesplit.Subnautica/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.1.1")]
-[assembly: AssemblyFileVersion("2.3.1.1")]
+[assembly: AssemblyVersion("2.3.2.0")]
+[assembly: AssemblyFileVersion("2.3.2.0")]
 
 [assembly: ComponentFactory(typeof(Factory))]
