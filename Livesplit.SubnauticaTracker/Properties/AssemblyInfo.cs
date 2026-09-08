c40cb61a91c3192c9507e8a92c2e282a2e17b58a
ItsFrostyYo <Kaleb.Austin0009@gmail.com>
2026-09-07 20:49:51 -0400

    Assembly Info Update

--- Livesplit.SubnauticaTracker/Properties/AssemblyInfo.cs
+++ Livesplit.SubnauticaTracker/Properties/AssemblyInfo.cs
@@ -3,12 +3,16 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Configurable Subnautica progression tracker for speedrunning.")]
-[assembly: AssemblyDescription("LiveSplit.SubnauticaTracker")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Fully Configurable Subnautica Progression tracker for Speedrunning that displays Blueprint, Databanks and Achievement Unlocks until 100% Completion and can Provide Logging for what's Missing.")]
+[assembly: AssemblyCompany("No Company")]
 [assembly: AssemblyProduct("LiveSplit.SubnauticaTracker")]
 [assembly: AssemblyCopyright("Copyright © Kaleb Austin 2026")]
+
 [assembly: ComVisible(false)]
 [assembly: Guid("6e869dbf-99e8-4b7c-9237-7192d6417d64")]
+
 [assembly: AssemblyVersion("1.6.3.0")]
 [assembly: AssemblyFileVersion("1.6.3.0")]
+[assembly: AssemblyInformationalVersion("1.6.3")]
+
 [assembly: ComponentFactory(typeof(LiveSplit.SubnauticaTracker.Factory))]
